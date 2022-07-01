5301f7d7948482c865046094b0a7a29c22a38646
Marco Busellato <marco.busellato@develondigital.com>
2022-07-01 14:11:21 +0200

    add Access-Control

--- dist/app.js
+++ dist/app.js
@@ -11,6 +11,12 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const auth_1 = __importDefault(require("./api/auth"));
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
+    next();
+});
 app.use('/api', auth_1.default);
 mongoose_1.default.connect(process.env.MONGODB_URI).then(() => {
     app.listen(8080);
--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,12 @@ import authRoutes from './api/auth'
 
 const app = express()
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization')
+  next()
+})
 
 app.use('/api', authRoutes)
 
