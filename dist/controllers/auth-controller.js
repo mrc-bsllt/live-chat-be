76c058b8e5ad14818ce56289e4cf7e2e2b51016c
Marco Busellato <marco.busellato@develondigital.com>
2022-07-05 11:33:51 +0200

    remove console.log

--- dist/controllers/auth-controller.js
+++ dist/controllers/auth-controller.js
@@ -33,6 +33,5 @@ const login = (req, res, next) => {
     if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
     }
-    console.log('ANDATA');
 };
 exports.login = login;
