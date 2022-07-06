5ba5214ce0456d488ccd19d32ee2e90b4f0f9586
Marco Busellato <marco.busellato@develondigital.com>
2022-07-06 14:27:46 +0200

    add multer - remove body-parser

--- dist/app.js
+++ dist/app.js
@@ -4,20 +4,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const body_parser_1 = __importDefault(require("body-parser"));
+const path_1 = __importDefault(require("path"));
+// import bodyParser from 'body-parser'
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const mongoose_1 = __importDefault(require("mongoose"));
+const multer_1 = __importDefault(require("multer"));
+const fileStorage = multer_1.default.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'src/storage/images');
+    },
+    filename: (req, file, callback) => {
+        callback(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+const fileFilter = (req, file, callback) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/webp') {
+        callback(null, true);
+    }
+    else {
+        callback(null, false);
+    }
+};
 const auth_1 = __importDefault(require("./api/auth"));
 const user_1 = __importDefault(require("./api/user"));
 const app = (0, express_1.default)();
-app.use(body_parser_1.default.json());
+app.use(express_1.default.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
     next();
 });
+app.use((0, multer_1.default)({ storage: fileStorage, fileFilter }).single('image_path'));
+app.use('/src/storage/images', express_1.default.static(path_1.default.join(__dirname, 'storage/images')));
 app.use('/api', auth_1.default);
 app.use('/api', user_1.default);
 mongoose_1.default.connect(process.env.MONGODB_URI).then(() => {
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,40 @@
 import express from 'express'
-import bodyParser from 'body-parser'
+import path from 'path'
 import { config } from 'dotenv'
 config()
 import mongoose from 'mongoose'
+import type { RequestMod } from './types/auth'
+
+import multer from 'multer'
+const fileStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'src/storage/images')
+  },
+  filename: (req, file, callback) => {
+    callback(null, new Date().toISOString() +  '-' + file.originalname)
+  }
+}) 
+const fileFilter = (req: RequestMod, file: Express.Multer.File, callback: multer.FileFilterCallback) => {
+  if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/webp') {
+    callback(null, true)
+  } else {
+    callback(null, false)
+  }
+}
 
 import authRoutes from './api/auth'
 import userRoutes from './api/user'
 
 const app = express()
-app.use(bodyParser.json())
+app.use(express.json())
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization')
   next()
 })
+app.use(multer({ storage: fileStorage, fileFilter }).single('image_path'))
+app.use('/src/storage/images', express.static(path.join(__dirname, 'storage/images')))
 
 app.use('/api', authRoutes)
 app.use('/api', userRoutes)
